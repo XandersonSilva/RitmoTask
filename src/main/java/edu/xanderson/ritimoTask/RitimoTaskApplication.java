38462e79f4009282e8ff12145c0ae7dbf05c0cb8
XandersonSilva <isacsilvasouza2@gmail.com>
2025-06-13 13:26:05 -0300

    Inicio da criação do sistema, configuração de variáveis de ambiente e dependências

---
+++ src/main/java/edu/xanderson/ritimoTask/RitimoTaskApplication.java
@@ -0,0 +1,13 @@
+package edu.xanderson.ritimoTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitimoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RitimoTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/xanderson/ritimoTask/RitimoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xanderson.ritimoTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitimoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
